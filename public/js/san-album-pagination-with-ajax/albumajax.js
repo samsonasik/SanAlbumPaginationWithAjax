9ef2b2953d61bce408e5e562dfdccfaec8ab16aa
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-02-11 18:35:02 +0700

    add conditional if no data

--- public/js/san-album-pagination-with-ajax/albumajax.js
+++ public/js/san-album-pagination-with-ajax/albumajax.js
@@ -17,7 +17,13 @@ function findAlbums(page)
          '<th>Artist</th>'+
          '<th>&nbsp;</th>'+
          '</tr>';
-     
+         
+         if (html.paginator.length==0) {
+             table += '<tr>'+
+               '<td colspan="3" style="text-align:center;">No Data Found</td>'+
+            '</tr>';
+         }
+      
          for (var i=0;i<html.paginator.length;i++) {
             table += '<tr>'+
                '<td>'+html.paginator[i].title+'</td>'+
@@ -30,61 +36,62 @@ function findAlbums(page)
          
          $(".loader").html(table);
          
-         //now, append with paginator partial
-         var pagerlink = '<div>'+
-                           '<ul class="pagination">';
-        
-        if (html.paginator_previous) {
-               pagerlink += '<!-- Previous page link -->'+
-                           '<li>'+
-                              '<a href="javascript:void(0);" onclick="findAlbums('+html.paginator_previous+');">'+
-                               '       &lt;&lt;'+
-                          ' </a>'+
-                       '</li>';
-        } else {
-            pagerlink += '<!-- Previous page link -->'+
-                           '<li>'+
-                              '<a onclick="javascript:void(0);return false;">'+
-                               '       &lt;&lt;'+
-                          ' </a>'+
-                       '</li>';
-        }
-        
-        for(var i=1;i<=html.paginator_lastPageInRange;i++) {
-            if (i== html.paginator_current) {
-               pagerlink +='<li class="active">'+
-                         '<a onclick="javascript:void(0);return false;">'+i+'</a>'+
-                     '</li>';
-            } else {
-               pagerlink +='<li>'+
-                         '<a href="javascript:void(0);" onclick="findAlbums('+i+');">'+i+
-                         '</a>'+
-                     '</li>';
-
-            }
-        }
-        
-      if (html.paginator_next) {
-               pagerlink += '<!-- Next page link -->'+
-                           '<li>'+
-                              '<a href="javascript:void(0);" onclick="findAlbums('+html.paginator_next+');">'+
-                               '       &gt;&gt;'+
-                          ' </a>'+
-                       '</li>';
-        } else {  
-            pagerlink += '<!-- Next page link -->'+
-                           '<li>'+
-                              '<a onclick="javascript:void(0);return false;">'+
-                               '       &gt;&gt;'+
-                          ' </a>'+
-                       '</li>';
-        }
-        
-         pagerlink +='</ul>'+
-     '</div>';
-
-      $(".loader").append(pagerlink);
-         
+         if (html.paginator.length>0) {
+               //now, append with paginator partial
+               var pagerlink = '<div>'+
+                                 '<ul class="pagination">';
+              
+              if (html.paginator_previous) {
+                     pagerlink += '<!-- Previous page link -->'+
+                                 '<li>'+
+                                    '<a href="javascript:void(0);" onclick="findAlbums('+html.paginator_previous+');">'+
+                                     '       &lt;&lt;'+
+                                ' </a>'+
+                             '</li>';
+              } else {
+                  pagerlink += '<!-- Previous page link -->'+
+                                 '<li>'+
+                                    '<a onclick="javascript:void(0);return false;">'+
+                                     '       &lt;&lt;'+
+                                ' </a>'+
+                             '</li>';
+              }
+              
+              for(var i=1;i<=html.paginator_lastPageInRange;i++) {
+                  if (i== html.paginator_current) {
+                     pagerlink +='<li class="active">'+
+                               '<a onclick="javascript:void(0);return false;">'+i+'</a>'+
+                           '</li>';
+                  } else {
+                     pagerlink +='<li>'+
+                               '<a href="javascript:void(0);" onclick="findAlbums('+i+');">'+i+
+                               '</a>'+
+                           '</li>';
+      
+                  }
+              }
+              
+            if (html.paginator_next) {
+                     pagerlink += '<!-- Next page link -->'+
+                                 '<li>'+
+                                    '<a href="javascript:void(0);" onclick="findAlbums('+html.paginator_next+');">'+
+                                     '       &gt;&gt;'+
+                                ' </a>'+
+                             '</li>';
+              } else {  
+                  pagerlink += '<!-- Next page link -->'+
+                                 '<li>'+
+                                    '<a onclick="javascript:void(0);return false;">'+
+                                     '       &gt;&gt;'+
+                                ' </a>'+
+                             '</li>';
+              }
+              
+               pagerlink +='</ul>'+
+           '</div>';
+      
+            $(".loader").append(pagerlink);
+         }
     }, 'json');
 }
 
